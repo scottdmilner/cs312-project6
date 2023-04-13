9a369ba6754f9fa0a40689ce82b3ba45852654d7
Scott Milner <scottdmilner@gmail.com>
2023-04-13 10:51:45 -0600

    remove commented code

--- TSPSolver.py
+++ TSPSolver.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 
-#from which_pyqt import PYQT_VER
-#if PYQT_VER == 'PYQT5':
-# 	from PyQt5.QtCore import QLineF, QPointF
-# elif PYQT_VER == 'PYQT4':
-# 	from PyQt4.QtCore import QLineF, QPointF
-# elif PYQT_VER == 'PYQT6':
 from PyQt6.QtCore import QLineF, QPointF
-# else:
-# 	raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
 
 import time
@@ -16,7 +8,6 @@ import numpy as np
 from TSPClasses import *
 from heapq import *
 import itertools
-from collections import deque
 from random import randrange
 
 
